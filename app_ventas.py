fb1986b188b7c03833562984b7a63181cd038d6f
ryuryoko1999-debug <ryuryoko1999@gmail.com>
2026-09-21 06:56:35 -0300

    Update app_ventas.py

--- app_ventas.py
+++ app_ventas.py
@@ -67,7 +67,7 @@ GID = "0"
 CSV_URL = f"https://docs.google.com/spreadsheets/d/{SHEET_ID}/export?format=csv&gid={GID}"
 
 # ⚠️ PEGA AQUÍ TU URL DE APPS SCRIPT DE LA PLANILLA MUEBLES
-SCRIPT_URL_MUEBLES = "https://script.google.com/macros/s/TU_SCRIPT_MUEBLES/exec"
+SCRIPT_URL_MUEBLES = "https://script.google.com/macros/s/AKfycbw3OPwzlrzvi-2zkX_qUyQG_xK3AltPc9J_iEHWkFwskoyfAeZBg_DvRqnMLokCdEY/exec"
 
 # --- PANEL LATERAL: AGREGAR NUEVO MUEBLE CON FOTO ---
 with st.sidebar:
